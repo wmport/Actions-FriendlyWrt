964133fb83277c8b2e6d51fba7bb79948169da9a
wmport <159494449+wmport@users.noreply.github.com>
2024-09-16 01:29:39 +0300

    Update custome_config.sh

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,4 +3,4 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
-sed -i 's/192.168.1.1/10.0.10.100/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.2.1/10.0.10.100/g' package/base-files/files/bin/config_generate

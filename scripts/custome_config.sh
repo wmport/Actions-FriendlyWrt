eee8c10540e1b72e642cf7705eed3cd8a638e521
wmport <159494449+wmport@users.noreply.github.com>
2024-09-17 19:41:33 +0300

    Update custome_config.sh
    change defailt IP

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,4 +3,4 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
-sed -i -e 's/192.168.2.1/10.0.10.100/g' configs/rockchip/01-nanopi
+sed -i 's/192.168.2.1/10.0.10.100/g' friendlywrt/target/linux/rockchip/armv8/base-files/etc/board.d/02_network
